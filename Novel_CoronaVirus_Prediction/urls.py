d11a0fc35bac4adf4340e9f1614c3e3bd8b89f98
Gokul Patil <72352930+gokul-patil0509@users.noreply.github.com>
2021-07-05 18:04:40 +0530

    Update urls.py

--- Novel_CoronaVirus_Prediction/urls.py
+++ Novel_CoronaVirus_Prediction/urls.py
@@ -3,6 +3,5 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('app/', include('Upcoming_Cases_Predictor.urls')),
+    path('', include('Upcoming_Cases_Predictor.urls')),
 ]
